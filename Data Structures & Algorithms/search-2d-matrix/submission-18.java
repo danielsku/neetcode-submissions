e76702f0b67bf928e9f331833e0103beb2eb41bb
Daniel Skurvidas <danielskurvidas@gmail.com>
2026-05-28 19:39:23 -0400

    Add: search-2d-matrix - submission-18

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-18.java
@@ -0,0 +1,37 @@
+// Resolve
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int l = 0;
+        int r = matrix.length - 1;
+        int row = 0;
+
+        while(l <= r){
+            row = l + (r-l)/2; // implement different version if it overflows
+            if(matrix[row][0] == target){
+                return true;
+            } else if(matrix[row][0] < target){
+                l = row + 1;
+            } else if(matrix[row][0] > target){
+                r = row - 1;
+            }
+        }
+
+        row = (l+r)/2;
+
+        l = 0;
+        r = matrix[row].length - 1; 
+        
+        while(l<=r){
+            int m = l + (r-l)/2;
+            if(matrix[row][m] == target){
+                return true;
+            } else if(matrix[row][m] < target){
+                l = m + 1;
+            } else if(matrix[row][m] > target){
+                r = m - 1;
+            }
+        }
+        return false;
+    }
+}
