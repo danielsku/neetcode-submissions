d6b0269e953342cd320e5c6fea027bfeba64a3fd
Daniel Skurvidas <danielskurvidas@gmail.com>
2026-06-29 18:02:31 -0400

    Add: kth-largest-integer-in-a-stream - submission-3

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-3.java
@@ -0,0 +1,26 @@
+class KthLargest {
+
+    int k;
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>();
+
+        for(int num : nums){
+            pq.offer(num);
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+    }
+    
+    public int add(int val) {
+        pq.offer(val);
+        if(pq.size() > k){
+            pq.poll();
+        }
+
+        return pq.peek();
+    }
+}
