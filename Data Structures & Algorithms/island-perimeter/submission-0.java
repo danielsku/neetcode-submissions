76499cea1dc8cddfdf69dadae1857403d05e4ade
Daniel Skurvidas <danielskurvidas@gmail.com>
2026-08-31 22:40:29 -0400

    Add: island-perimeter - submission-0

---
+++ Data Structures & Algorithms/island-perimeter/submission-0.java
@@ -0,0 +1,37 @@
+class Solution {
+    int[][] grid;
+    boolean[][] isVisited;
+    private int rows, cols;
+
+    public int islandPerimeter(int[][] grid) {
+        this.grid = grid;
+        this.rows = grid.length;
+        this.cols = grid[0].length;
+        this.isVisited = new boolean[rows][cols];
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(grid[i][j] == 1) return dfs(i, j);
+            }
+        }
+        
+        return 0;
+    }
+
+    private int dfs(int i, int j){
+        if(i < 0 
+        || j < 0 
+        || i >= rows 
+        || j >= cols 
+        || grid[i][j] == 0) return 1;
+
+        if(isVisited[i][j]) return 0;
+
+        isVisited[i][j] = true;
+        return dfs(i + 1, j) + 
+        dfs(i - 1, j) + 
+        dfs(i, j + 1) + 
+        dfs(i, j - 1);
+
+    }
+}
